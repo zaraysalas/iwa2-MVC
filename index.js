6272e834d9515f28b22154ab8e5507347da3d1a0
zaraysalas <salas-lopez@hotmail.com>
2021-04-20 14:05:02 +0000

    heroku version port fixed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv = require('dotenv');
 
 //------------Server-----------
 var app = express();
-var port = 8000;
+var port = process.env.PORT || 8000;//8000
 dotenv.config();
 
 app.use(bodyParser.json());
